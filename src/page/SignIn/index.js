5ca7849033e21c8462442465cb1cb6afe8b23fb6
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-20 23:43:45 -0300

    configurando tela html teste validaçao de input

--- src/page/SignIn/index.js
+++ src/page/SignIn/index.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 // import { Container } from './styles';
 
-function SignIn() {
-  return <h1>LOGIN</h1>;
-}
+export default function SignIn() {
+  const [valor, setValor] = useState([]);
+  const [newValor, setNewValor] = useState("");
 
-export default SignIn;
+  //função que verifica se a quantidade de numeros é menor que 10 ou maior que 11
+  function handleAdd() {
+    if (newValor.length < 10 || newValor.length > 11) {
+      alert("Erro, apenas MO ou CPF");
+      return;
+    }
+
+    setValor([...valor, newValor]);
+    console.log(newValor);
+    setNewValor("");
+  }
+
+  return (
+    <>
+      <ul>
+        {valor.map((v) => (
+          <li key={v}>
+            {v.length === 10
+              ? (v = "MO")
+              : v.length === 11
+              ? (v = "CPF")
+              : (v = "Não tem")}
+          </li>
+        ))}
+      </ul>
+      {/* <form> */}
+      <input
+        type="text"
+        name="cpfOuMo"
+        placeholder="CPF/MO"
+        value={newValor}
+        onChange={(e) => setNewValor(e.target.value)}
+      />
+      <br /> <br />
+      <button type="submit" onClick={handleAdd}>
+        Acessar
+      </button>
+      {/* </form> */}
+    </>
+  );
+}
